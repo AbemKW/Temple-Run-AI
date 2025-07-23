8ff37f36a0306e8efa18936310398c976f8a7902
Abem Kibatu <abemkibatu101@gmail.com>
2025-07-23 12:20:37 -0500

    Add genome retrieval and setting methods to NeuralNetwork class

--- model.py
+++ model.py
@@ -19,6 +19,32 @@ class NeuralNetwork:
         self.biases_hidden = np.random.uniform(-1, 1, (self.hidden_nodes))
         self.biases_output = np.random.uniform(-1, 1, (self.output_nodes))
     
+    def get_genome(self):
+        
+        genome = np.concatenate([
+            self.weights_input_hidden.flatten(),
+            self.weights_hidden_output.flatten(),
+            self.biases_hidden,
+            self.biases_output
+        ])
+        return genome
+    
+    def set_genome(self, genome):
+        idx = 0
+        
+        size = self.input_nodes * self.hidden_nodes
+        self.weights_input_hidden = genome[idx:idx + size].reshape(self.hidden_nodes, self.input_nodes)
+        idx += size
+
+        size = self.hidden_nodes * self.output_nodes
+        self.weights_hidden_output = genome[idx:idx + size].reshape(self.output_nodes, self.hidden_nodes)
+        idx += size
+
+        self.biases_hidden = genome[idx:idx + self.hidden_nodes]
+        idx += self.hidden_nodes
+
+        self.biases_output = genome[idx:idx + self.output_nodes]
+
     def sigmoid(self, x):
         """Sigmoid activation function"""
         return 1 / (1 + np.exp(-np.clip(x, -500, 500)))  # Clip to prevent overflow
