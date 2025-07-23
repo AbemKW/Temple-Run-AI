db53b0c71543c4eeac5ad469df12cd0dc3565ae3
Abem Kibatu <abemkibatu101@gmail.com>
2025-07-22 23:14:25 -0500

    Refactor Player class to integrate AI decision-making; add think method and state retrieval for obstacle avoidance

--- player.py
+++ player.py
@@ -1,14 +1,20 @@
 import pygame
 import constants
+from model import NeuralNetwork
 
 
 class Player:
-    def __init__(self):
+    def __init__(self, brain = False):
         self.score = 0
+        self.fitness = 0
         self.player_lane = 1
         self.survival_time = 0
         self.obstacle_avoided = 0
         self.game_over = False
+        if(brain):
+            self.brain = brain
+        else:
+            self.brain = NeuralNetwork(5,10,3)
         self.player_rect = pygame.Rect(
             constants.LANE_X[self.player_lane] - constants.PLAYER_WIDTH // 2, 
             constants.PLAYER_Y, 
@@ -16,8 +22,16 @@ class Player:
             constants.PLAYER_HEIGHT
         )
 
-    def move_player(self, direction):
+    def think(self, game_state):
         """Move player left (-1) or right (1)"""
+        state = self.get_state(game_state)
+        action = self.brain.predict(state)
+        if action == 1:  # Move left
+            self.move_player(-1)
+        elif action == 2:  # Move right
+            self.move_player(1)
+
+    def move_player(self, direction):
         new_lane = self.player_lane + direction
         if 0 <= new_lane < constants.NUM_LANES:
             self.player_lane = new_lane
@@ -34,3 +48,27 @@ class Player:
     def draw_player(self, screen):
         """Optimized player drawing using pre-calculated rect"""
         pygame.draw.rect(screen, constants.PLAYER_COLOR, self.player_rect)
+
+    def get_state(self, game_state):
+        """Get the current state for AI decision making"""
+        player_lane = self.player_lane  # index
+        one_hot_location = [0, 0, 0]
+        one_hot_location[player_lane] = 1
+        
+        if game_state.obstacles:
+            ahead_obstacles = [o for o in game_state.obstacles if o['y'] < self.player_rect.y]
+            if ahead_obstacles:
+                closest = max(ahead_obstacles, key=lambda o: o['y'])
+                try:
+                    obstacle_lane = constants.LANE_X.index(closest['x'])
+                except ValueError:
+                    obstacle_lane = -1
+                obstacle_distance = self.player_rect.y - closest['y']
+            else:
+                obstacle_lane = -1
+                obstacle_distance = 999
+        else:
+            obstacle_lane = -1
+            obstacle_distance = 999
+
+        return one_hot_location + [obstacle_lane, obstacle_distance]
